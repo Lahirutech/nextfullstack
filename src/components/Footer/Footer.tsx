25b7eb27d70d6df817879b66eb3186098ee8c7d8
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-07 13:07:46 +0530

    feat:adding footer with assets and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import styles from './footer.navbar.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div className={styles.socialIcons}>
+        <Link href=''>
+          <Image
+            src='/assets/img/1.png'
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt='Lama Dev Facebook Account'
+          />
+        </Link>
+        <Link href=''>
+          <Image
+            src='/assets/img/2.png'
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt='Lama Dev Facebook Account'
+          />
+        </Link>
+        <Link href=''>
+          <Image
+            src='/assets/img/3.png'
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt='Lama Dev Facebook Account'
+          />
+        </Link>
+        <Link href=''>
+          <Image
+            src='/assets/img/4.png'
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt='Lama Dev Facebook Account'
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
