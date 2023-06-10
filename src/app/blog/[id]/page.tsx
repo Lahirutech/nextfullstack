b412481c6211d405cc396921f85c87a478455ebe
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-10 08:15:17 +0530

    feat:add the blog posts apis

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import styles from './singlepost.module.css';
 import Image from 'next/image';
-const BlogPost = () => {
-  const data: any = {};
+import { notFound } from 'next/navigation';
+import { PostsType } from '../page';
+
+const getSinglePost = async (id: string) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    return notFound();
+  }
+  return res.json();
+};
+
+const BlogPost = async ({ params }: { params: { id: string } }) => {
+  const data: PostsType = await getSinglePost(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.desc}</p>
+          <p className={styles.desc}>{data.body}</p>
           <div className={styles.author}>
             <Image
-              src={data.img}
+              src={'/assets/img/prof.webp'}
               alt=''
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>{data.username}</span>
+            <span className={styles.username}>Lanka</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src={data.img}
+            src={'/assets/img/apps.jpg'}
             alt=''
             fill={true}
             className={styles.image}
@@ -30,7 +43,7 @@ const BlogPost = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>{data.content}</p>
+        <p className={styles.text}></p>
       </div>
     </div>
   );
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,31 +3,49 @@ import Link from 'next/link';
 import React from 'react';
 import styles from './blog.module.css';
 
-const Blog = () => {
-  const item: any = {};
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch Data');
+  }
+  const json = await res.json();
+  return json;
+}
+
+export type PostsType = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      {/* {data.map((item) => ( */}
-      <Link
-        href={`/blog/${item._id}`}
-        className={styles.container}
-        key={item.id}
-      >
-        <div className={styles.imageContainer}>
-          <Image
-            src={item.img}
-            alt=''
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>{item.title}</h1>
-          <p className={styles.desc}>{item.desc}</p>
-        </div>
-      </Link>
-      {/* ))} */}
+      {data.map((item: PostsType) => (
+        <Link
+          href={`/blog/${item.id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src={'/assets/img/apps.jpg'}
+              alt=''
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
