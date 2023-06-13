83005d39328194668ee8a66ab75d7a387e4e4ca1
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-13 09:31:09 +0530

    fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -26,31 +26,7 @@ export type PostsType = {
 
 const Blog = async () => {
   const data = await getData();
-  return (
-    <div className={styles.mainContainer}>
-      {data.map((item: PostsType) => (
-        <Link
-          href={`/blog/${item._id}`}
-          className={styles.container}
-          key={item.id}
-        >
-          <div className={styles.imageContainer}>
-            <Image
-              src={item.img}
-              alt=''
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.content}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+  return <div className={styles.mainContainer}></div>;
 };
 
 export default Blog;
