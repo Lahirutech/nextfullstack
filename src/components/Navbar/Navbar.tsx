81506d4f8e4a1d8ef49c6a502c056984db3b415e
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-13 07:55:12 +0530

    fix:navabr changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,41 +1,44 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
 import styles from './navbar.module.css';
 import DarkmodeToggle from '../darkmodeToggle/DarkmodeToggle';
-
+import { signOut, useSession } from 'next-auth/react';
+const links = [
+  {
+    id: 1,
+    title: 'Home',
+    url: '/',
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    url: '/portfolio',
+  },
+  {
+    id: 3,
+    title: 'Blog',
+    url: '/blog',
+  },
+  {
+    id: 4,
+    title: 'About',
+    url: '/about',
+  },
+  {
+    id: 5,
+    title: 'Contact',
+    url: '/contact',
+  },
+  {
+    id: 6,
+    title: 'Dashboard',
+    url: '/dashboard',
+  },
+];
 const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      title: 'Home',
-      url: '/',
-    },
-    {
-      id: 2,
-      title: 'Portfolio',
-      url: '/portfolio',
-    },
-    {
-      id: 3,
-      title: 'Blog',
-      url: '/blog',
-    },
-    {
-      id: 4,
-      title: 'About',
-      url: '/about',
-    },
-    {
-      id: 5,
-      title: 'Contact',
-      url: '/contact',
-    },
-    {
-      id: 6,
-      title: 'Dashboard',
-      url: '/dashboard',
-    },
-  ];
+  const session = useSession();
+
   return (
     <div className={styles.navContainer}>
       <div className={styles.companyName}> Company name</div>
@@ -49,7 +52,14 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
-        <button className={styles.logoutBtn}>Log Out</button>
+        {session.status === 'authenticated' && (
+          <button
+            className={styles.logoutBtn}
+            onClick={() => signOut()}
+          >
+            Log Out
+          </button>
+        )}
       </div>
     </div>
   );
