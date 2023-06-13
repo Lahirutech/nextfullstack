eb0ee2793dea47ac195b55618aeed96885e00782
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-13 07:50:37 +0530

    Post route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -12,3 +12,15 @@ export const GET = async (request: NextApiRequest) => {
     return new NextResponse('Database Error', { status: 500 });
   }
 };
+
+export const POST = async (request: Request) => {
+  const body = await request.json();
+  const newPost = new Post(body);
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse('Post has been created', { status: 201 });
+  } catch (err) {
+    return new NextResponse('Database Error', { status: 500 });
+  }
+};
