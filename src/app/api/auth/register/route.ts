cb4939fe1b8087045738067c019739d51cb0e2ba
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-13 07:33:08 +0530

    feat:user register route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -8,21 +8,31 @@ export const POST = async (request: Request) => {
   const { name, email, password } = await request.json();
   await connect();
   const hashedPassword = await bcrypt.hash(password, 5);
-
   const newUser = new User({
     name,
     email,
     password: hashedPassword,
   });
+  const user = await User.find({ email });
 
-  try {
-    await newUser.save();
-    return new NextResponse('User has been created', {
-      status: 201,
-    });
-  } catch (err: any) {
-    return new NextResponse(err.message, {
-      status: 500,
-    });
+  if (user.length > 0) {
+    console.log('user already exist');
+    return new NextResponse(
+      JSON.stringify({ message: 'User already exists' }),
+      {
+        status: 403,
+      }
+    );
+  } else {
+    try {
+      await newUser.save();
+      return new NextResponse('User has been created', {
+        status: 201,
+      });
+    } catch (err: any) {
+      return new NextResponse(err, {
+        status: 500,
+      });
+    }
   }
 };
